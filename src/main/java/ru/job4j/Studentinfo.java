b8ea8d04a89d2243fb25d66c27fd7999fca35fdc
Александр <1686@inbox.ru>
2022-06-25 14:39:51 +0500

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class studentinfo {
-    public static void main (String[] args) {
+public class Studentinfo {
+    public static void main(String[] args) {
         System.out.println("Alexander Ermolaev");
         System.out.println("06.01.1986");
     }
