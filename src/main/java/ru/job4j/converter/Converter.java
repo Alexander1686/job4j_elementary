812ff9b35f7c7582b4acc2a4c8da3da9b7b55522
Александр <1686@inbox.ru>
2023-05-15 23:53:18 +0500

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,32 +2,34 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static double rubleToEuro(double value) {
-        double rsl = value / 70;
-        return (double) rsl;
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
     }
 
-    public static double rubleToDollar(double value) {
-        double rsl = value / 60;
-        return (double) rsl;
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return rsl;
+
     }
 
     public static void main(String[] args) {
-        double euro = Converter.rubleToEuro(140);
-        double dollar = Converter.rubleToDollar(140);
+        float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollar + " dollars.");
 
-        double in = 140;
-        double ind = 140;
-        double expected = 2;
-        double expecteddollar = 2.3333333333333335;
-        double out = Converter.rubleToEuro(in);
-        double outdollar = Converter.rubleToDollar(ind);
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
-        boolean passeddollar = expecteddollar == outdollar;
         System.out.println("140 rubles are 2. Test result : " + passed);
-        System.out.println("140 rubles are 2.3333333333333335. Test result : " + passeddollar);
+
+        float dollar = Converter.rubleToDollar(120);
+        System.out.println("120 rubles are " + dollar + " dollars.");
+
+        in = 120;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("120 rubles are 2. Test result : " + passed);
 
     }
 }
