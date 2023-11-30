972f68000fd5045aab2d5801ff22d8a2d8b80805
Александр <1686@inbox.ru>
2023-12-01 03:23:37 +0500

    5.7. Ипотека [#156315 #459676]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ package ru.job4j.loop;
   public static int year(double amount, int salary, double percent) {
       int year = 0;
         while (amount > 0) {
-            amount = amount + (amount * percent);
+            amount += amount * percent;
             amount -= salary;
             year++;
       }
