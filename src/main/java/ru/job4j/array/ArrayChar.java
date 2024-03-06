b0aeebe7e472041d05758ec2082fd93eff2e95ee
Александр <1686@inbox.ru>
2024-03-07 00:54:42 +0500

    6.4.0. Слово начинается с ... [#41585]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,20 +5,19 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
         for (int i = 0; i < prefix.length; i++) {
-            System.out.println(prefix[i]);
             if (prefix[i] != word[i]) {
                 result = false;
                 break;
             }
-    }
+        }
         return result;
-}
+    }
 
     public static void main(String[] args) {
-        char[] word = new char[] {'h', 'e', 'l', 'l', 'o'};
-        char[] prefix = new char[] {'h', 'e'};
-        char[] prefix2 = new char[] {'h', 'i'};
-      System.out.println(ArrayChar.startsWith(word, prefix));
+        char[] word = new char[]{'h', 'e', 'l', 'l', 'o'};
+        char[] prefix = new char[]{'h', 'e'};
+        char[] prefix2 = new char[]{'h', 'i'};
+        System.out.println(ArrayChar.startsWith(word, prefix));
         System.out.println(ArrayChar.startsWith(word, prefix2));
     }
 }
