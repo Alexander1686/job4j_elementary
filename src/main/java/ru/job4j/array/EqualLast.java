8d2c51a502111de9558235037b6ef56587bba132
Александр <1686@inbox.ru>
2024-03-27 22:51:13 +0500

    6.4.1. Сравнить последние элементы двух массивов [#304228 #485121]

--- src/main/java/ru/job4j/array/EqualLast.java
+++ src/main/java/ru/job4j/array/EqualLast.java
@@ -2,20 +2,13 @@ package ru.job4j.array;
 
 public class EqualLast {
     public static boolean check(int[] left, int[] right) {
-        boolean result = true;
-        for (int i = left.length - 1; i > left.length - 2; i--) {
-            if (left[left.length - 1] != right[right.length - 1]) {
-               result = false;
-               break;
-            }
-        }
-        return result;
+        return left[left.length - 1] == right[right.length - 1];
     }
 
     public static void main(String[] args) {
-        int[] left = new int[] {1, 2, 3};
-        int[] right = new int[] {6, 4, 3};
-        int[] right2 = new int[] {3, 3, 4};
+        int[] left = new int[]{1, 2, 3};
+        int[] right = new int[]{6, 4, 3};
+        int[] right2 = new int[]{3, 3, 4};
         System.out.println(check(left, right));
         System.out.println(check(left, right2));
     }
