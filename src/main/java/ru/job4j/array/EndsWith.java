82afbf59dc6a557a083d656f13415e4f7e452253
Александр <1686@inbox.ru>
2024-04-06 14:11:47 +0500

    6.4.3. Слово заканчивается на ... [#156317 #485984]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] postfix) {
         boolean result = true;
-        for (int i = word.length - 1; i > word.length - 3; i--) {
-            if (word[i] != postfix[i]) {
-                result = false;
-                break;
+        for (int i = 0; i < postfix.length; i++) {
+                if (word[word.length - 1 - i] != postfix[postfix.length - 1 - i]) {
+                    result = false;
+                    break;
             }
 
         }
@@ -15,8 +15,8 @@ public class EndsWith {
 
     public static void main(String[] args) {
         char[] word = new char[] {'h', 'e', 'l', 'l', 'o'};
-        char[] postfix = new char[] {'h', 'e', 'l', 'l', 'o'};
-        char[] postfix2 = new char[] {'h', 'e', 'l', 'l', 'a'};
+        char[] postfix = new char[] {'l', 'o'};
+        char[] postfix2 = new char[] {'l', 'a'};
         System.out.println(EndsWith.endsWith(word, postfix));
         System.out.println(EndsWith.endsWith(word, postfix2));
     }
