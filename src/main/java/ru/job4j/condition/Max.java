62cb1a4917cc798df7c4c83cd9ac04fcaba8b7b9
Александр <1686@inbox.ru>
2024-09-01 23:45:32 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,8 +6,22 @@ public class Max {
         return result;
     }
 
+    public static int max(int left, int right, int up) {
+        int result = up > right && up > left ? up : max(left, right);
+        return result;
+    }
+
+    public static int max(int left, int right, int up, int down) {
+        int result = down > right && down > left && down > up ? down : max(left, right, up);
+        return result;
+    }
+
     public static void main(String[] args) {
         int rsl = max(1, 2);
+        System.out.println(rsl);
+        rsl = max(1, 2, 3);
+        System.out.println(rsl);
+        rsl = max(1, 2, 3, 4);
         System.out.println(rsl);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,27 @@ class MaxTest {
         assertThat(result).isEqualTo(expected);
         System.out.println(result);
     }
+
+    @Test
+    void whenMax3To2To1Then3() {
+        int left = 1;
+        int right = 2;
+        int up = 3;
+        int result = Max.max(left, right, up);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+        System.out.println(result);
+    }
+
+    @Test
+    void whenMax4To3To2To1Then3() {
+        int left = 1;
+        int right = 2;
+        int up = 3;
+        int down = 4;
+        int result = Max.max(left, right, up, down);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+        System.out.println(result);
+    }
 }
